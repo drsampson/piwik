drsampson/piwik

commit b92971f7371a34cf9f8593e7610a605b88f1ff19

refs #2708  * removing providers' descriptions from language file  * adding providers' logo  * fixing "API returned an error: The parameter 'idSite=' is missing from the request." when a normal user queries MultiSites.getOne  * displaying website name in SMS content even when MultiSites.getAll returns one website
 TODO
  * integration tests for SMS content
  * using POST instead of GET to send SMS to go around a Clockwork limitation

git-svn-id: http://dev.piwik.org/svn/trunk@6758 59fd770c-687e-43c8-a1e3-f5a4ff64c105

plugins/MultiSites/API.php
@@ -188,6 +188,11 @@ class Piwik_MultiSites_API
 		{
 			$dataTable = $dataTable->mergeChildren();
 		}
+		else
+		{
+			$firstDataTableRow = $dataTable->getFirstRow();
+			$firstDataTableRow->setColumn('label', $sites);
+		}
 
 		// if the period isn't a range & a lastN/previousN date isn't used, we get the same
 		// data for the last period to show the evolution of visits/actions/revenue

plugins/MobileMessaging/API.php
@@ -290,13 +290,6 @@ class Piwik_MobileMessaging_API
 		$this->setCurrentUserSettings($settings);
 	}
 
-	private function saveSMSSentCount($smsSentCount)
-	{
-		$settings = $this->getCurrentUserSettings();
-		$settings[Piwik_MobileMessaging::SMS_SENT_COUNT_OPTION] = $smsSentCount;
-		$this->setCurrentUserSettings($settings);
-	}
-
 	/**
 	 * validate phone number
 	 *

plugins/MobileMessaging/ReportRenderer/Sms.php
@@ -117,6 +117,7 @@ class Piwik_MobileMessaging_ReportRenderer_Sms extends Piwik_ReportRenderer
 		$smarty->assign("reportRowsMetadata", $reportRowsMetadata);
 		$smarty->assign("prettyDate", $prettyDate);
 		$smarty->assign("siteHasECommerce", $siteHasECommerce);
+		$smarty->assign("displaySiteName", $processedReport['metadata']['action'] == 'getAll');
 
 		$this->rendering .= $smarty->fetch(PIWIK_USER_PATH . '/plugins/MobileMessaging/templates/SMSReport.tpl');
 	}

plugins/MobileMessaging/SMSProvider.php
@@ -24,7 +24,7 @@ abstract class Piwik_MobileMessaging_SMSProvider
 	const MAX_UCS2_CHARS_IN_ONE_CONCATENATED_SMS = 67;
 
 	static public $availableSMSProviders = array(
-		'Clockwork' => 'MobileMessaging_SMSProvider_Description_Clockwork',
+		'Clockwork' => 'You can use <img src="/plugins/MobileMessaging/images/Clockwork.png"/>to send SMS Reports from Piwik.<br/> * First, get an API Key form <a href="http://clockworksms.com/platforms/piwik/">Clockwork</a> (Signup is free with some test messages) <br/> * Put your Clockwork API Key in Piwik. <br/> About Clockwork <br/> * Clockwork gives you fast, reliable high quality worldwide SMS delivery, over 450 networks in every corner of the globe. <br/>  * Cost per SMS message is around ~0.08USD (0.06EUR). </br>  * Most countries and networks are supported but we suggest you check the latest position on their coverage map here <a href="http://www.clockworksms.com/sms-coverage/">www.clockworksms.com/sms-coverage/</a>',
 	);
 
 	/**

plugins/MobileMessaging/lang/en.php
@@ -49,6 +49,5 @@ $translations = array(
 	'MobileMessaging_MobileReport_MobileMessagingSettingsLink' => 'the Mobile Messaging settings page',
 	'MobileMessaging_MobileReport_AdditionalPhoneNumbers' => 'You can add more phone numbers by accessing',
 	'MobileMessaging_SMS_Content_Too_Long' => '[too long]',
-	'MobileMessaging_MultiSites_Must_Be_Activated' => 'To generate SMS texts of your website stats, please enable the MultiSites plugin in Piwik.',
-	'MobileMessaging_SMSProvider_Description_Clockwork' => 'You can use Clockwork.com to send SMS Reports from Piwik.<br/> * First, get an API Key form <a href="http://clockworksms.com/platforms/piwik/">Clockwork</a> (Signup is free with some test messages) <br/> * Put your Clockwork API Key in Piwik. <br/> About Clockwork <br/> * Clockwork gives you fast, reliable high quality worldwide SMS delivery, over 450 networks in every corner of the globe. <br/>  * Cost per SMS message is around ~0.08USD (0.06EUR). </br>  * Most countries and networks are supported but we suggest you check the latest position on their coverage map here <a href="www.clockworksms.com/sms-coverage/">www.clockworksms.com/sms-coverage/</a>',
+	'MobileMessaging_MultiSites_Must_Be_Activated' => 'To generate SMS texts of your website stats, please enable the MultiSites plugin in Piwik.'
 );

plugins/MobileMessaging/templates/SMSReport.tpl
@@ -10,8 +10,7 @@
 		{assign var=rowMetrics value=$row->getColumns()}
 		{assign var=rowMetadata value=$reportRowsMetadata[$rowId]->getColumns()}
 
-		{*website name (if there is more than one site)*}
-		{if !$smarty.foreach.reportRows.first or !$smarty.foreach.reportRows.last}
+		{if $displaySiteName}
 			{$rowMetrics.label}:{literal} {/literal}
 		{/if}
 

plugins/MobileMessaging/templates/Settings.tpl
@@ -32,7 +32,7 @@
 
 		{foreach from=$smsProviders key=smsProvider item=description}
 			<p class='providerDescription' id='{$smsProvider}'>
-				{$description|translate}
+				{$description}
 			</p>
 		{/foreach}
 

commit 421a687bf25f5288a3b6af60def1e41e73507943

refs #2708 fixing regression introduced in r6758
git-svn-id: http://dev.piwik.org/svn/trunk@6761 59fd770c-687e-43c8-a1e3-f5a4ff64c105

plugins/MultiSites/API.php
@@ -190,8 +190,11 @@ class Piwik_MultiSites_API
 		}
 		else
 		{
-			$firstDataTableRow = $dataTable->getFirstRow();
-			$firstDataTableRow->setColumn('label', $sites);
+			if(!$dataTable instanceof Piwik_DataTable_Array)
+			{
+				$firstDataTableRow = $dataTable->getFirstRow();
+				$firstDataTableRow->setColumn('label', $sites);
+			}
 		}
 
 		// if the period isn't a range & a lastN/previousN date isn't used, we get the same
@@ -250,6 +253,10 @@ class Piwik_MultiSites_API
 		{
 			$getNameFor = array('Piwik_Site', 'getNameFor');
 			$dataTable->filter('ColumnCallbackReplace', array('label', $getNameFor));
+		}
+		else
+		{
+			$dataTable->filter('ColumnDelete', array('label'));
 		}
 
 		// replace record names with user friendly metric names